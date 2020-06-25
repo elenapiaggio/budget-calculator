00336614e59f96fd41581701273a0f6ea4ea7294
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-25 21:20:10 +0200

    add container styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div>
+    <Container>
       <Header title="Budget Calculator" />
-    </div>
+      <ContainerForm/>
+    </Container>
   );
 }
 
