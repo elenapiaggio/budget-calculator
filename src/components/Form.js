59aa5b2559fe02b8fd0796e68d35aee77b58348f
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-25 22:46:18 +0200

    ADD styled componnets to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,39 +1,61 @@
 import React from "react";
+import styled from '@emotion/styled';
+
+const Field = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
 
 const Form = () => {
   return (
     <form>
-      <div>
-        <label>Bootcamp</label>
-        <select>
-          <option value="">Select</option>
+      <Field>
+        <Label>Bootcamp</Label>
+        <Select>
           <option value="web">Web developoment</option>
           <option value="ux">UX-UI</option>
           <option value="big">Big Data</option>
-        </select>
-      </div>
-      <div>
-        <label>Type</label>
-        <select>
+        </Select>
+      </Field>
+      <Field>
+        <Label>Type</Label>
+        <Select>
           <option value="ft">Full time</option>
           <option value="pt">Part time</option>
           <option value="fton">Full time online</option>
           <option value="pton">Part time online</option>
-        </select>
-      </div>
-      <div>
-        <label>Plan</label>
-        <input
+        </Select>
+      </Field>
+      <Field>
+        <Label>Plan</Label>
+        <InputRadio
           type='radio'
           name='discount'
           value='normal'
         /> Basic
-        <input
+        <InputRadio
           type='radio'
           name='discount'
           value='normal'
         /> Full
-      </div>
+      </Field>
       <button type='button'>Calculator</button>
     </form>
   );
