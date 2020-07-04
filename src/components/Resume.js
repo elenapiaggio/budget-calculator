744858c14d10d61128f9f17c19c4ff622ae53e3e
Elena Piaggio <elenapiaggio@gmail.com>
2020-07-05 00:15:53 +0200

    add text reult in resume component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,28 @@
 import React, { Fragment } from "react";
+import styled from '@emotion/styled';
+import { changeToUpperCase } from './Helper';
+
+const ContentResume = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838F;
+  color: #FFF;
+  margin-top: 1rem;
+`;
 
 const Resume = ({ data }) => {
   const { bootcamp, type, plan } = data;
   console.log("data", {data})
 
   return (
-    <Fragment>
+    <ContentResume>
       <h2>Resume</h2>
       <ul>
-        <li>Bootcamp: {bootcamp}</li>
-        <li>Type: {type}</li>
-        <li>Plan: {plan}</li>
+        <li>Bootcamp: {changeToUpperCase(bootcamp)}</li>
+        <li>Type: {changeToUpperCase(type)}</li>
+        <li>Plan: {changeToUpperCase(plan)}</li>
       </ul>
-    </Fragment>
+    </ContentResume>
   );
 };
 
